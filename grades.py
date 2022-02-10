425a112067938426cf1130befbe23bcb2aa909e8
jack11wagner <jacksonjw519@gmail.com>
2022-02-10 08:57:51 -0500

    Update Docstring

--- grades.py
+++ grades.py
@@ -1,7 +1,9 @@
-""" 
-Compute the average grade for list of assignments
-"""
 def compute_hw_average(grades):
+
+"""
+Compute the average grade for list of assignments
+Param: grades
+"""
     if len(grades) == 0:
         return 0
     return grades[0]
