ffd9926f34792ce3964145d4c02639d83fce8722
Jack Wagner <jacksonjw519@gmail.com>
2022-02-15 17:52:17 -0500

    fixed indenting error

--- grades.py
+++ grades.py
@@ -1,9 +1,8 @@
 def compute_hw_average(grades):
-
-"""
-Compute the average grade for list of assignments
-Param: grades
-"""
+    """
+    Compute the average grade for list of assignments
+    Param: grades
+    """
     if len(grades) == 0:
         return 0
     return grades[0]
